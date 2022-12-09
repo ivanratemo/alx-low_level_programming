982a475146687db5d6cfddb4ab63dd838d0d83bd
Ivanratemo <ivanratemo@gmail.com>
2022-12-09 06:14:22 -0800

    Advance task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - returns a combination of numbers
@@ -10,7 +11,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; j++)
+	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
