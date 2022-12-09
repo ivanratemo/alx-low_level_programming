0a102b9d665b704b52d230437de48abe3323e07d
Ivanratemo <ivanratemo@gmail.com>
2022-12-09 03:32:53 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= '2')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
