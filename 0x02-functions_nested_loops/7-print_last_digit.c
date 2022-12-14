de61a6eb30b9dd0de5a1bfed38a67979f36e3b00
Ivanratemo <ivanratemo@gmail.com>
2022-12-14 11:13:21 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,13 @@
 
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 	{
 		last_digit = (-1 * (n % 10));
 		_putchar (last_digit + '0');
-		return (print_last_digit);
+		return (last_digit);
 	}
 
 	else
