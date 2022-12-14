0252fa9d0b94416b730c938ef3cab4f5227538e9
Ivanratemo <ivanratemo@gmail.com>
2022-12-14 13:11:10 -0800

    Advance task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,8 +34,14 @@ void print_times_table(int n)
 				}
 				else if (product > 9 && product < 100)
 				{
+					_putchar(' ');
+					_putchar('0' + ((product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
 					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + ((product /10) % 10));
 					_putchar('0' + (product % 10));
 				}
 			}
